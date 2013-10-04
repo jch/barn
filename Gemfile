b6a6e50e1d792a55b3805bbed92b66a37bfd589a
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-10-03 18:42:15 -0700

    Bye bye debugger

--- Gemfile
+++ Gemfile
@@ -2,11 +2,3 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in barn.gemspec
 gemspec
-
-group :debug do
-  if RUBY_VERSION > "1.9"
-    gem "debugger"
-  else
-    gem "ruby-debug"
-  end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,2 @@
 require "barn"
 require "minitest/autorun"
-
-# http://about.travis-ci.org/docs/user/languages/ruby/#Exclude-non-essential-gems-like-ruby-debug-from-your-Gemfile
-unless ENV["CI"]
-  if RUBY_VERSION > "1.9"
-    require "debugger"
-  else
-    require "ruby-debug"
-  end
-end
