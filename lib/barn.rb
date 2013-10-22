49af0dd6b3eb5f204baf70314e58f3cdcd7a7b60
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-10-22 13:00:35 -0700

    Add TomDoc

--- lib/barn.rb
+++ lib/barn.rb
@@ -6,7 +6,22 @@ require "barn/helpers"
 require "barn/builders/hash"
 
 module Barn
+  # Public: Error raised when a Barn::Factory has already been defined.
+  #
+  # Examples
+  #
+  #   > Barn.define(:user)
+  #   => #<Barn::Factory:0x007fb4fd2ca998...>
+  #   > Barn.define(:user)
+  #   Barn::DuplicateFactoryError: factory `user' already defined
   class DuplicateFactoryError < StandardError; end
+
+  # Public: Error raised when building an undefined Barn::Factory.
+  #
+  # Examples
+  #
+  #   > Barn.build(:user)
+  #   Barn::UndefinedFactoryError: factory `user' not defined
   class UndefinedFactoryError < StandardError; end
 
   extend Namespace
